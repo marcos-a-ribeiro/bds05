6584abd90a580b1c6c5ce5c185ed6b351fd38918
Marcos Antonio Ribeiro <maribeiro9600@gmail.com>
2022-05-03 18:30:39 -0300

    Entities, secutiry

---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_user (email, name, password) VALUES ('ana@gmail.com', 'ana', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (email, name, password) VALUES ('bob@gmail.com', 'bob', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
